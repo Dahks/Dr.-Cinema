01ceb3e864c5598b413cc7e2bf0bc17af22c6356
dagur-thoris <dagurtoris@gmail.com>
2023-12-06 14:54:33 +0000

    initial project skeleton

---
+++ DrCinema/.eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "standard-with-typescript",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "@typescript-eslint/semi": ["error", "always"], // Enforce semicolons at the end of lines
+    // "semi": ["error", "always"], // This line enforces the use of semicolons
+    "@typescript-eslint/comma-dangle": ["error", "only-multiline"],
+    "@typescript-eslint/quotes": ["error", "double", { avoidEscape: true }],
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/return-await": ["error", "in-try-catch"],
+    "prettier/prettier": "error",
+  },
+};
---
+++ DrCinema/App.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import Navigator from "./src/routes/index";
+
+const App: React.FC = () => <Navigator />;
+export default App;
