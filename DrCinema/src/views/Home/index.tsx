77221d032ab08661a30edc6cba87f94fbbb8cc39
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2023-12-11 13:32:30 -0500

    setup useState counter

--- DrCinema/src/views/Home/index.tsx
+++ DrCinema/src/views/Home/index.tsx
@@ -1,12 +1,32 @@
 import { Button, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Txt from "../../components/Txt";
 import { type HomeProps } from "../../routes";
+import styles from "../../styles/styles";
 
 const Home = ({ navigation, route }: HomeProps) => {
+  const [counter, setCounter] = useState<number>(0);
   return (
     <View>
       <Txt>Home screen</Txt>
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{ width: 80, backgroundColor: "#222232", borderRadius: 12 }}
+        >
+          <Button
+            color={"#fff"}
+            title={`${counter}`}
+            onPress={() => {
+              setCounter(counter + 1);
+            }}
+          />
+        </View>
+      </View>
       <Button
         title="Go to Other Window"
         onPress={() => {
