63ff87c5a97bb41775e4250fae1f19473f079bad
Kristófer Fannar Björnsson <kristoferfannarb@gmail.com>
2023-12-12 12:29:42 -0500

    fetch and display all cinemas + Cinema model added

--- DrCinema/src/models/Cinema.ts
+++ DrCinema/src/models/Cinema.ts
@@ -1,23 +1,23 @@
-export type Cinema = {
-  id: string,
-  name: string,
-  description: string,
-  address: string, // could be converted to Address type
-  phone: string,
-  websiteUrl: string,
-  googleMap: string,
-};
+export interface Cinema {
+  id: string;
+  name: string;
+  description: string;
+  address: string; // could be converted to Address type
+  phone: string;
+  websiteUrl: string;
+  googleMap: string;
+}
 
-export type APICinema = {
-  address: string,
-  city: string,
-  name: string,
-  phone: string,
-  description: string,
-  website: string,
-  id: string,
-  google_map: string,
-};
+export interface APICinema {
+  address: string;
+  city: string;
+  name: string;
+  phone: string;
+  description: string;
+  website: string;
+  id: string;
+  google_map: string;
+}
 
 export const toCinema = (apiCinema: APICinema) => {
   const cinema: Cinema = {
